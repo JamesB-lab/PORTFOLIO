ecd646b90851dbd778a028dda87d37287b721c46
Borja Lazaro Toralles <borjalazarotoralles@gmail.com>
2022-11-19 00:01:08 +0000

    Adds contact email backend

---
+++ portfolio/email.py
@@ -0,0 +1,47 @@
+from django import forms
+from django.conf import settings
+from django.core.mail import send_mail
+
+# TODO: add validation in the form using validators=[...]
+
+
+class EmailForm(forms.Form):
+    name = forms.CharField(label='Name')
+    company = forms.CharField(label='Company')
+    email = forms.CharField(label='E-mail')
+    phone = forms.CharField(label='Phone')
+    location = forms.CharField(label='Location',
+                               widget=forms.Select(
+                                   choices=[
+                                       ('remote', 'Remote'),
+                                       ('hybrid', 'Hybrid'),
+                                       ('onsite', 'Onsite'),
+                                   ]
+                               ))
+    message = forms.CharField(label='Subject', widget=forms.Textarea)
+
+    def send_email(self):
+        # From the form
+        name = self['name'].value()
+        company = self['company'].value()
+        their_email = self['email'].value()
+        phone = self['phone'].value()
+        location = self['location'].value()
+        message = self['message'].value()
+        # Configured
+        subject = 'Portfolio contact form'
+        noreply_email = 'James Booth <noreply@mail.nuggetmuffin.tech>'
+        my_email = 'j.r.booth01@gmail.com'
+        full_message = f'Message from: {name}\n'
+        f'Company: {company}'
+        f'Email: {their_email}'
+        f'Phone: {phone}'
+        f'Location: {location}'
+        f'Message:\n{message}'
+        send_mail(
+            subject=subject,
+            message=full_message,
+            from_email=noreply_email,
+            recipient_list=[their_email, my_email],
+            fail_silently=False,
+        )

9c07b1405028b0fa317703a67dc7e6540c28a302
Borja Lazaro Toralles <borjalazarotoralles@gmail.com>
2023-02-22 22:44:26 +0000

    Added portfolio skill tags

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,23 +1,7 @@
 from django.db import models
-from django.utils.translation import gettext_lazy
 
-
-class Skills(models.Model):
-    python = models.BooleanField()
-    excel = models.BooleanField()
 
 class Post(models.Model):
-
-    # class SkillTag(models.TextChoices):
-    #     PYTHON = 'PYTHON', gettext_lazy('Python')
-    #     EXCEL = 'EXCEL', gettext_lazy('Excel')
-    #     R = 'R', gettext_lazy('R')
-    #     SQL = 'SQL', gettext_lazy('SQL')
-    #     AWS = 'AWS', gettext_lazy('AWS')
-    #     RPI = 'RPI', gettext_lazy('Raspberry Pi')
-    #     ML = 'ML', gettext_lazy('Machine Learning')
-    #     DESCRIPTIVE_ANALYTICS = 'DESCRIPTIVE_ANALYTICS', gettext_lazy('Descriptive analytics')
-    #     DJANGO = 'DJANGO', gettext_lazy('Django')
 
     title = models.CharField(max_length=200)
     author = models.ForeignKey(
@@ -27,7 +11,16 @@ class Post(models.Model):
     body = models.TextField()
     imagePath = models.CharField(max_length=200)
     thumbnail = models.TextField()
-    skills = Skills()
+    
+    # Skills
+    skill_python = models.BooleanField(default=False)
+    skill_excel = models.BooleanField(default=False)
+    skill_r = models.BooleanField(default=False)
+    skill_sql = models.BooleanField(default=False)
+    skill_aws = models.BooleanField(default=False)
+    skill_raspberry_pi = models.BooleanField(default=False)
+    skill_machine_learning = models.BooleanField(default=False)
+    skill_descriptive_analytics = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title

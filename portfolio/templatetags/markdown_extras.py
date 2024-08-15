b69d80bec1d3be76406a8714dc62a0723a4f57cc
Borja Lazaro Toralles <borjalazarotoralles@gmail.com>
2024-08-15 18:06:12 +0100

    Fixed signing images in post details from markdown using staticfiles_storage.url()

--- portfolio/templatetags/markdown_extras.py
+++ portfolio/templatetags/markdown_extras.py
@@ -1,12 +1,22 @@
 import markdown
 from django import template
 from django.template.defaultfilters import stringfilter
+from django.contrib.staticfiles.storage import staticfiles_storage
+import re
 
 register = template.Library()
 
 md = markdown.Markdown(extensions=['extra'])
 
+pattern = r'src=\"/static/([^\"]+)\"'
+
+def sign_func(match) -> str:
+    captured_value = match.group(1)
+    return f'src="{staticfiles_storage.url(captured_value)}"'
+
 @register.filter()
 @stringfilter
 def markdown(value):
-    return md.convert(value)
+    text = md.convert(value)
+    signed_text = re.sub(pattern, sign_func, text)
+    return signed_text
